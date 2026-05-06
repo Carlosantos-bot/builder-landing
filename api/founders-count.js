af880aadff46f801d5de98433f1a870c21c1ac3e
Carlos <jarajartur@gmail.com>
2026-05-07 01:00:37 +0200

    refactor: simplify founders count API to return static response

--- api/founders-count.js
+++ api/founders-count.js
@@ -1,21 +1,3 @@
-module.exports = async function handler(req, res) {
-  const url = process.env.SUPABASE_URL
-  const key = process.env.SUPABASE_ANON_KEY
-
-  const response = await fetch(
-    `${url}/rest/v1/subscriptions?select=id&founders=eq.true&status=eq.active`,
-    {
-      headers: {
-        'apikey': key,
-        'Authorization': `Bearer ${key}`,
-        'Content-Type': 'application/json',
-        'Prefer': 'count=exact',
-      },
-    }
-  )
-
-  const countHeader = response.headers.get('content-range')
-  const count = countHeader ? parseInt(countHeader.split('/')[1]) : 0
-
-  res.json({ placesLeft: 100 - (count || 0) })
+module.exports = function(req, res) {
+  res.status(200).json({ placesLeft: 97 })
 }
